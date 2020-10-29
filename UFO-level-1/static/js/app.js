615817f0672aac8e319a293a2eeb1b748e1f8800
Julie Pyle <drjuliepyle@gmail.com>
2020-10-29 11:03:30 -0400

    Added entries to table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,22 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+data.forEach(function(UFOSighting) {
+    console.log(UFOSighting);
+    });
+
+data.forEach(function(UFOSighting) {
+    console.log(UFOSighting);
+    var row = tbody.append("tr");
+      });
+
+data.forEach(function(UFOSighting) {
+    console.log(UFOSighting);
+    var row = tbody.append("tr");
+
+    Object.entries(UFOSighting).forEach(function([key, value]) {
+        console.log(key, value);
+      });
+    });
