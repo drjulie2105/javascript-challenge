53f861692fa1ec965ad53f0bf576a92d81356608
Julie Pyle <drjuliepyle@gmail.com>
2020-11-01 13:33:54 -0500

    Added filter search code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -34,3 +34,44 @@ data.forEach(function(UFOSighting) {
         cell.text(value);
         });
       });
+  
+// Assign the data from `data.js` to a descriptive variable
+var sighting = data;
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+
+button.on("click", function() {
+  d3.select("tbody").html("");
+
+  d3.event.preventDefault();
+
+  var inputElement = d3.select("#datetime");
+
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+
+  var filteredData = sighting.filter(sightingDate => sightingDate.datetime === inputValue);
+
+  console.log(filteredData);
+
+  filteredData.forEach ((UFOSighting) => {
+    var row = tbody.append ('tr');
+
+    Object.entries(UFOSighting).forEach (([key, value]) => {
+      console.log (key, value);
+      var cell = row.append ('td');
+      cell.text(value);
+    });
+  });
+
+});
+
+
+
+
+  
+
+  
